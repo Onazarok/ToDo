b2f5f1f106d902080aab3d22613413a0ddd03539
Oleg <onazarok@icloud.com>
2019-08-14 20:29:44 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Oleg Nazarok on 8/14/19.
+//  Copyright © 2019 Oleg Nazarok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
